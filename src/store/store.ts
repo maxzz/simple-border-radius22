b478a17f7d68940cfccf3ba854744d283b2f2d98
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-07 00:33:50 -0700

    WIP. store. define required parameters

--- src/store/store.ts
+++ src/store/store.ts
@@ -38,3 +38,102 @@ namespace Storage {
 } //namespace Storage
 
 export const DarkShemaAtom = atomWithCallback(Storage.initialData.dark, Storage.save);
+
+///////////////
+
+const enum ShowRects {
+    none,
+    css,
+    svg
+}
+
+type Options = {
+    showControls: boolean;
+
+    shapes: number,
+    borderWidth: number,
+    scale: number,
+    shiftX: number,
+    shiftY: number,
+    symmetrical: boolean;
+
+    showRects: ShowRects, // 0 - none; 1 - CSS; 2 - SVG
+    showCssRects: boolean;
+    showSvgRects: boolean;
+    showBorder: boolean;
+    showSvgFrame: boolean;
+    showOnlyOneRect: boolean;
+    animate: boolean,
+    demoMode: boolean,
+};
+
+const defaultOptions: Options = {
+    showControls: true,
+
+    shapes: 1,
+    borderWidth: 1,
+    scale: .1,
+    shiftX: 20,
+    shiftY: 20,
+    symmetrical: true,
+
+    showRects: 0, // 0 - none; 1 - CSS; 2 - SVG
+    showCssRects: true,
+    showSvgRects: true,
+    showBorder: true,
+    showSvgFrame: true,
+    showOnlyOneRect: true,
+    animate: false,
+    demoMode: false,
+};
+
+const demoOptions: Options = {
+    showControls: true,
+    
+    shapes: 10,
+    borderWidth: 1,
+    scale: .1, // scale: .0043,
+    shiftX: 9,
+    shiftY: 7,
+    symmetrical: true,
+    
+    showRects: 0,
+    showCssRects: true,
+    showSvgRects: true,
+    showBorder: true,
+    showSvgFrame: true,
+    showOnlyOneRect: false,
+    animate: true,
+    demoMode: true,
+};
+
+function random(min: number, max: number): number {
+    return Math.floor(min + Math.random() * (max - min));
+}
+
+function generateShape(symmetrical: boolean) {
+    let wTL = random(5, 96);
+    let wBL = random(5, 96);
+    let hTL = random(5, 96);
+    let hTR = random(5, 96);
+
+    let wTR, wBR, hBL, hBR;
+
+    if (symmetrical) {
+        wTR = 100 - wTL;
+        wBR = 100 - wBL;
+        hBL = 100 - hTL;
+        hBR = 100 - hTR;
+    } else {
+        wTR = random(5, 96);
+        wBR = random(5, 96);
+        hBL = random(5, 96);
+        hBR = random(5, 96);
+    }
+
+    return `${wTL}% ${wTR}% ${wBR}% ${wBL}% / ${hTL}% ${hTR}% ${hBR}% ${hBL}%`;
+}
+
+function generateCorners(borderRadius: string) {
+    return [...(borderRadius || '').matchAll(/(\d\d*%)/g)].map((m) => m[1]);
+}
