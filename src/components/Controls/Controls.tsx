0b834710c4616e59de000100e43e35b18a446099
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-07 17:00:00 -0700

    WIP. Controls. add all old checkboxes

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 import { Atom, PrimitiveAtom, useAtom, useAtomValue, useSetAtom } from 'jotai';
 import { UISlider } from '../UI/UISlider';
 import { SliderProps } from '@radix-ui/react-slider';
-import { generatorOptions, uiOptions } from '@/store/store';
+import { generatorOptions, uiOptions, viewOptions } from '@/store/store';
 
 function SliderGroup({ label, valueAtom, ...rest }: { label: string; valueAtom: PrimitiveAtom<number>; } & SliderProps) {
     const [value, setValue] = useAtom(valueAtom);
     return (
         <label className="flex flex-col space-y-1 whitespace-nowrap">
             <div className="">{label}</div>
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-1">
                 <UISlider value={[value]} onValueChange={(v: number[]) => setValue(v[0])} {...rest} />
-                <div className="">{value}</div>
+                <div className="min-w-[4ch] text-[0.65rem]">{value}</div>
             </div>
+        </label>
+    );
+}
+
+function Checkbox({ label, valueAtom, ...rest }: { label: string; valueAtom: PrimitiveAtom<boolean>; } & HTMLAttributes<HTMLLabelElement>) {
+    const [value, setValue] = useAtom(valueAtom);
+    return (
+        <label className="flex items-center space-x-1 select-none" {...rest}>
+            <input
+                className="form-checkbox rounded text-primary-500 focus:ring-primary-500"
+                type="checkbox"
+                checked={value}
+                onChange={() => setValue((v) => !v)}
+            />
+            <div className="">{label}</div>
         </label>
     );
 }
@@ -22,17 +37,26 @@ export function Controls() {
         <div className="p-2 frame-box text-sm space-y-2">
             <SliderGroup label="Number of shapes" min={1} max={20} valueAtom={generatorOptions.shapesAtom} />
             <SliderGroup label="Shape border width" min={1} max={100} valueAtom={generatorOptions.borderWidthAtom} />
-            <SliderGroup label="Step scale" min={0.0001} max={2} step={0.0001} valueAtom={generatorOptions.scaleAtom} />
-            
+            <SliderGroup label="Step scale" min={0.001} max={2} step={0.001} valueAtom={generatorOptions.scaleAtom} />
+
             <SliderGroup label="Step x shift" min={-50} max={50} step={1} valueAtom={generatorOptions.shiftXAtom} />
             <SliderGroup label="Step y shift" min={-50} max={50} step={1} valueAtom={generatorOptions.shiftYAtom} />
 
-            <label className="flex items-center space-x-1">
-                <input className="form-checkbox rounded" type="checkbox" />
-                <div className="">Generate symmetrical</div>
-            </label>
+            <Checkbox label="Generate symmetrical" valueAtom={generatorOptions.symmetricalAtom} />
 
-            <hr/>
+            <div className="py-2">
+                <hr />
+            </div>
+
+            <fieldset className="px-4 border rounded">
+                <legend className="px-0.5">Show options</legend>
+                <div className="pt-1 pb-2 space-y-1">
+                    <Checkbox label="Show CSS corner rectangles" valueAtom={viewOptions.showCssRectsAtom} />
+                    <Checkbox label="Show SVG corner rectangles" valueAtom={viewOptions.showSvgRectsAtom} />
+                    <Checkbox label="Show SVG frame" valueAtom={viewOptions.showSvgFrameAtom} />
+                    <Checkbox label="Show borders" valueAtom={viewOptions.showBorderAtom} />
+                </div>
+            </fieldset>
         </div>
     </>);
 }
