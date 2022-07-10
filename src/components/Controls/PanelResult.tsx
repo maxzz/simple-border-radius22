f8fae9629896fab175fe507d470e2c6226bc0b2d
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-10 15:07:34 -0700

    WIP. PanelResult. add small preview

--- src/components/Controls/PanelResult.tsx
+++ src/components/Controls/PanelResult.tsx
@@ -1,5 +1,5 @@
-import { borderRadiusesAtom } from '@/store/store';
-import { borderRadiusesStr } from '@/store/store-utils';
+import { borderRadiusesAtom, generatorOptions } from '@/store/store';
+import { borderCSSProps, borderRadiusesArr, borderRadiusesStr } from '@/store/store-utils';
 import { classNames } from '@/utils/classnames';
 import { useAtomValue } from 'jotai';
 import React, { HTMLAttributes } from 'react';
@@ -7,9 +7,27 @@ import React, { HTMLAttributes } from 'react';
 export function PanelResult({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     const borderRadiuses = useAtomValue(borderRadiusesAtom);
 
+    const borderWidth = useAtomValue(generatorOptions.borderWidthAtom);
+
+    const corners = borderRadiusesArr(borderRadiuses);
+    const cornersCss = borderCSSProps(corners, borderWidth);
+    const cornersStr = borderRadiusesStr(borderRadiuses);
+
     return (
-        <div className={classNames("text-sm", className)} {...rest}>
-            {`border-radius: ${borderRadiusesStr(borderRadiuses)};`}
+        <div className={classNames("text-sm flex items-center justify-between", className)} {...rest}>
+            <div>
+                {`border-radius: ${cornersStr};`}
+            </div>
+            <div className="relative w-12 h-12 bg-red-300/40">
+                <div
+                    className="absolute inset-1 bubba"
+                    style={{
+                        borderRadius: cornersStr,
+                        borderStyle: 'solid',
+                        borderWidth: borderWidth,
+                    }}
+                />
+            </div>
         </div>
     );
 }
