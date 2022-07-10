f723611642c34cc9d3b0926d11e0ad53618ed56b
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-10 02:11:39 -0700

    WIP. EditorCanvas. fix border parts size by outer border not inner

--- src/components/Editor/EditorCanvas.tsx
+++ src/components/Editor/EditorCanvas.tsx
@@ -1,15 +1,15 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, HTMLAttributes } from 'react';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { borderRadiusesAtom, generatorOptions, viewOptions } from '@/store/store';
 import { borderRadiusesArr, borderRadiusesStr } from '@/store/store-utils';
 import './EditorCanvas.scss';
 import { classNames } from '@/utils/classnames';
 
-function RectsCss({ corners }: { corners: number[]; }) {
+function RectsCss({ corners, className, ...rest }: { corners: number[]; } & HTMLAttributes<HTMLDivElement>) {
     const showCssRects = useAtomValue(viewOptions.showCssRectsAtom);
     return (<>
         {showCssRects &&
-            <div className="markers">
+            <div className={classNames("markers", className)} {...rest}>
                 <div className="css-marker bm-tl" title={`top-left: ${corners[0]}%, ${corners[4]}%`}> </div>
                 <div className="css-marker bm-tr" title={`top-right: ${corners[1]}%, ${corners[5]}%`}> </div>
                 <div className="css-marker bm-br" title={`bottom-right: ${corners[2]}%, ${corners[6]}%`}> </div>
@@ -33,38 +33,35 @@ function Bubba() {
         '--h1': `${corners[5]}%`,
         '--h2': `${corners[6]}%`,
         '--h3': `${corners[7]}%`,
-        // '--border-width': `${borderWidth}px`,
     } as CSSProperties;
 
     const showBorder = useAtomValue(viewOptions.showBorderAtom);
 
     return (
-        <div
-            className={classNames(
-                "absolute inset-2 bg-red-500/20",
-                "bubba",  showBorder && "bubba-borders",
-            )}
-            style={{
-                borderRadius: borderRadiusesStr(borderRadiuses),
-                borderStyle: 'solid',
-                borderWidth: borderWidth,
-                ...cornersCss,
-            }}
-        >
-            <RectsCss corners={corners} />
+        <div className="absolute inset-0" style={cornersCss}>
+            <div
+                className={classNames(
+                    "absolute inset-2 bg-red-500/20",
+                    "bubba", showBorder && "bubba-borders",
+                )}
+                style={{
+                    borderRadius: borderRadiusesStr(borderRadiuses),
+                    borderStyle: 'solid',
+                    borderWidth: borderWidth,
+                }}
+            />
+            <RectsCss className="absolute inset-2" corners={corners} />
         </div>
     );
 }
 
 export function CanvasContainer() {
     return (<>
-        <div
-            className="relative frame-box frame-square bg-[color:var(--tm-ui-bg)]">
+        <div className="relative frame-box frame-square bg-[color:var(--tm-ui-bg)]">
 
             <Bubba />
 
-            <div className="absolute left-2 bottom-2">
-            </div>
+            {/* <div className="absolute left-2 bottom-2"> </div> */}
         </div>
     </>);
 }
