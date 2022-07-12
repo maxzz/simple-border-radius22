e2c645448c5a3d2283acf6e13b51ebbb5af35659
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-11 19:20:18 -0700

    WIP. PanelResult. add preview multiple steps

--- src/components/Controls/PanelResult.tsx
+++ src/components/Controls/PanelResult.tsx
@@ -1,24 +1,19 @@
 import { borderRadiusesAtom, generatorOptions } from '@/store/store';
-import { borderRadiusesStr, getBubbaTransform } from '@/store/store-utils';
+import { borderRadiusesStr, generateTransforms } from '@/store/store-utils';
 import { classNames } from '@/utils/classnames';
 import { useAtomValue } from 'jotai';
 import React, { HTMLAttributes } from 'react';
 
-function Preview({ cornersStr }: { cornersStr: string; }) {
-    const scale = useAtomValue(generatorOptions.scaleAtom);
-    const shiftX = useAtomValue(generatorOptions.shiftXAtom);
-    const shiftY = useAtomValue(generatorOptions.shiftYAtom);
-    const nShapes = useAtomValue(generatorOptions.nShapesAtom);
-    const shapes = Array(nShapes).fill(0);
+function Preview({ cornersStr, shapes }: { cornersStr: string; shapes: string[] }) {
     return (<>
-        {shapes.map((_, idx) => (
+        {shapes.map((transform, idx) => (
             <div
                 className="absolute inset-1 bubba"
                 style={{
                     borderRadius: cornersStr,
                     borderStyle: 'solid',
                     borderWidth: 1,
-                    transform: getBubbaTransform(idx, scale, shiftX, shiftY),
+                    transform,
                 }}
                 key={idx}
             />
@@ -31,6 +26,13 @@ export function PanelResult({ className, ...rest }: HTMLAttributes<HTMLDivElemen
     const borderRadiuses = useAtomValue(borderRadiusesAtom);
     const cornersStr = borderRadiusesStr(borderRadiuses);
     //const borderWidth = useAtomValue(generatorOptions.borderWidthAtom);
+
+    const scale = useAtomValue(generatorOptions.scaleAtom);
+    const shiftX = useAtomValue(generatorOptions.shiftXAtom);
+    const shiftY = useAtomValue(generatorOptions.shiftYAtom);
+    const nShapes = useAtomValue(generatorOptions.nShapesAtom);
+
+    const shapes = generateTransforms(nShapes, scale, shiftX, shiftY);
     return (
         <div className={classNames("text-sm flex items-center justify-between", className)} {...rest}>
             <div>
@@ -38,7 +40,7 @@ export function PanelResult({ className, ...rest }: HTMLAttributes<HTMLDivElemen
             </div>
 
             <div className="relative w-12 h-12 bg-red-300/40 overflow-hidden">
-                <Preview cornersStr={cornersStr} />
+                <Preview cornersStr={cornersStr} shapes={shapes} />
             </div>
         </div>
     );
